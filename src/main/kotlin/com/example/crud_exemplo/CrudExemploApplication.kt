7d5e56f43cf09cedc4486943318218a399bb6993
Matheus Cortez <matheus.cortez@live.com>
2024-02-26 20:27:58 -0300

    chore: initial commit

---
+++ src/main/kotlin/com/example/crud_exemplo/CrudExemploApplication.kt
@@ -0,0 +1,11 @@
+package com.example.crud_exemplo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CrudExemploApplication
+
+fun main(args: Array<String>) {
+	runApplication<CrudExemploApplication>(*args)
+}
